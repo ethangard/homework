66803d9a12993ff032ed56a2f786ece82bb66c41
ethanol <ethan.gard@jane.app>
2022-12-01 15:50:36 -0500

    finished project and stretch

--- homework_7/hangman/script.js
+++ homework_7/hangman/script.js
@@ -1,149 +1,188 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const keyboard = document.getElementById('keyboard');
-const mysteryWord = 'popcorn'.toUpperCase();
-const guessSelector = document.getElementById('guess-count')
+const randomWords = [
+  'boxes',
+  'headphones',
+  'stranger',
+  'popcorn',
+  'microwave',
+  'mustard',
+  'cabinets',
+  'star',
+  'rapscallion',
+  'pirate',
+  'carpool',
+  'bovine',
+  'cookie',
+];
+const mysteryWord =
+  randomWords[Math.floor(Math.random() * randomWords.length)].toUpperCase();
 
+console.log(`The mystery word is:`, mysteryWord);
+const guessSelector = document.getElementById('guess-count');
 const image = document.getElementById('image');
+const testImg = document.getElementById('hang-image');
 
 let guessCount = 0;
-
 let correctGuess = 0;
 
+// Event Listeners
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    /* Word Render */
+    let wordArr = mysteryWord.split('');
+    let letterOutput = '';
 
-// Event Listeners 
-document.addEventListener('DOMContentLoaded', () => {
-  /* Word Render */
-  let wordArr = mysteryWord.split('');
-
-  let letterOutput = '';
-
-  wordArr.forEach((wordLetter) => {
-    letterOutput += `<div class="word-letter-block">${wordLetter.toUpperCase()}</div>`;
-  });
-
-  document.getElementById('word').innerHTML = letterOutput;
-
-  /* Word Renders  */
-
-  let output = '';
-
-  const arrAlpha = alphabet.split('');
-
-  arrAlpha.forEach((letter) => {
-    output += `<div class="letter-block">${letter.toUpperCase()}</div>`;
-  });
-
-  keyboard.innerHTML = output;
-
-  // Click event
-
-  const selectLetterBlocks = document.querySelectorAll('.letter-block');
-
-  // console.log(selectLetterBlocks);
-
-  const wordLetterBlockAll = document.querySelectorAll('.word-letter-block');
-
-
-/* Testing Keyboard physical keyboard inputs */
-
-  // Lowercase between inclusive 97 - 122
-  // Uppercase between inclusive 65 - 90
-
-  document.addEventListener('keypress', (e) => {
-    if (e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode >= 97 && e.keyCode <= 122) {
-
-      console.log(`Your input key was: ${e.key}. The keycode was: ${e.keyCode}`)   
-      
-      
-
-      
-
-
-
-
-      
-    }
-  });
-
-
-
-  /* Test End */
-
-  selectLetterBlocks.forEach((letter) => {
-    letter.addEventListener('click', (e) => {
-      // console.log(`Event Target: ${e.target}`);
-      e.target.classList.add('active');
-
-      /* If Block with Logic  */
-
-      if (wordArr.includes(e.target.innerHTML)) {
-        wordLetterBlockAll.forEach((letter) => {
-          if (letter.innerHTML == e.target.innerHTML) {
-            letter.classList.add('show');
-            correctGuess++;
-          }
-        });
-      } else {
-        guessCount++;
-        image.innerHTML = `<img src="./images/gallows${guessCount}.jpg" alt="" id="hang-image"></img>`;
+    wordArr.forEach((wordLetter) => {
+      if (wordLetter !== ' ') {
+        letterOutput += `<div class="word-letter-block">${wordLetter.toUpperCase()}</div>`;
       }
-
-      console.log('Current correct guesses', correctGuess);
-
-      setTimeout(() => {
-        if (guessCount >= 6) {
-          alert('Game over, try again!');
-          window.location.reload();
-        }
-
-        if (correctGuess === mysteryWord.length) {
-          alert(`You win! 🎉 The mystery word was ${mysteryWord}.`);
-          new Audio(``);
-          window.location.reload();
-        }
-      }, 100);
-
-      // wordLetterBlockAll.forEach((letter)=> {
-      // Testing
-      // console.log(wordArr);
-      // console.log(e.target.innerHTML);
-      // console.log(letter.innerHTML);
-
-      //   if (letter.innerHTML == e.target.innerHTML) {
-
-      //     // const selectedLettersArr = [];
-      //     // for (let i = 0; i < wordArr.length; i++) {
-      //     //   if (wordArr[i] === e.target.innerHTML) selectedLettersArr.push(i)
-      //     // }
-
-      //     //console.log(letter)
-      //     letter.classList.add('show')
-      //     // console.log(letter)
-
-      //    } else {
-      //       guessCount++;
-      //       image.innerHTML = `<img src="./images/gallows${guessCount}.jpg" alt="" id="hang-image"></img>`
-      //    }
-      // })
-
-      // console.log(`My Current Event ... removing listner for: `, e.target)
-      // Remove event listener
-      letter.removeEventListener('click', (e) => {
-        e.target;
-      });
-
-      guessSelector.innerHTML = guessCount;
     });
-  });
 
-  // Show selected letters
+    document.getElementById('word').innerHTML = letterOutput;
 
-  // show selected letters
+    /* Word Renders  */
 
-  // const wordLetterBlockAll = document.querySelectorAll('word-letter-block');
+    let output = '';
 
-  // wordLetterBlockAll.forEach((letter)=> {
+    const arrAlpha = alphabet.split('');
 
-  // })
-}, {once: true });
+    arrAlpha.forEach((letter) => {
+      output += `<div class="letter-block">${letter.toUpperCase()}</div>`;
+    });
 
+    keyboard.innerHTML = output;
+
+    // Click event
+
+    const selectLetterBlocks = document.querySelectorAll('.letter-block');
+
+    // console.log(selectLetterBlocks);
+
+    const wordLetterBlockAll = document.querySelectorAll('.word-letter-block');
+
+    /* Testing Keyboard physical keyboard inputs */
+
+    // Lowercase between inclusive 97 - 122
+    // Uppercase between inclusive 65 - 90
+
+    const pressedKeys = [];
+
+    document.addEventListener('keypress', (e) => {
+      if (
+        (e.keyCode >= 65 && e.keyCode <= 90) ||
+        (e.keyCode >= 97 && e.keyCode <= 122)
+      ) {
+        if (!pressedKeys.includes(e.keyCode)) {
+          if (e.keyCode >= 65 && e.keyCode <= 90) {
+            pressedKeys.push(e.keyCode);
+            pressedKeys.push(e.keyCode + 32);
+          }
+
+          if (e.keyCode >= 97 && e.keyCode <= 122) {
+            pressedKeys.push(e.keyCode);
+            pressedKeys.push(e.keyCode - 32);
+          }
+          selectLetterBlocks.forEach((letter) => {
+            if (letter.innerHTML === e.key.toUpperCase()) {
+              letter.classList.add('active');
+            }
+          });
+
+          if (wordArr.includes(e.key.toUpperCase())) {
+            wordLetterBlockAll.forEach((letter) => {
+              if (letter.innerHTML == e.key.toUpperCase()) {
+                letter.classList.add('show');
+                correctGuess++;
+                console.log(`Correct Guess is:`, correctGuess);
+              }
+            });
+          } else {
+            guessCount++;
+            // image.innerHTML = `<img src="./images/gallows${guessCount}.jpg" alt="" id="hang-image"></img>`;
+            testImg.src = `./images/gallows${guessCount}.jpg`;
+          }
+
+          setTimeout(() => {
+            if (guessCount >= 6) {
+              const defeat = new Audio('./audio/marine.m4a');
+              defeat.play();
+              setTimeout(() => {
+                alert(
+                  `Game over, the correct word was ${mysteryWord}, try again!`
+                );
+                window.location.reload();
+              }, 300);
+            }
+
+            if (correctGuess === mysteryWord.length) {
+              const victory = new Audio('./audio/open_chest.m4a');
+              victory.play();
+              setTimeout(() => {
+                alert(`You win! 🎉 The mystery word was ${mysteryWord}.`);
+
+                window.location.reload();
+              }, 300);
+            }
+          }, 100);
+
+          guessSelector.innerHTML = guessCount;
+        }
+      }
+    });
+
+    selectLetterBlocks.forEach((letter) => {
+      letter.addEventListener(
+        'click',
+        (e) => {
+          e.target.classList.add('active');
+          if (wordArr.includes(e.target.innerHTML)) {
+            wordLetterBlockAll.forEach((letter) => {
+              if (letter.innerHTML == e.target.innerHTML) {
+                letter.classList.add('show');
+                correctGuess++;
+                console.log(`Correct Guess is:`, correctGuess)
+              }
+            });
+          } else {
+            guessCount++;
+            testImg.src = `./images/gallows${guessCount}.jpg`;
+          }
+
+          setTimeout(() => {
+            if (guessCount >= 6) {
+              const defeat = new Audio('./audio/marine.m4a');
+              defeat.play();
+              setTimeout(() => {
+                alert(
+                  `Game over, the correct word was ${mysteryWord}, try again!`
+                );
+                window.location.reload();
+              }, 300);
+            }
+
+            if (correctGuess === mysteryWord.length) {
+              const victory = new Audio('./audio/open_chest.m4a');
+              victory.play();
+              setTimeout(() => {
+                alert(`You win! 🎉 The mystery word was ${mysteryWord}.`);
+
+                window.location.reload();
+              }, 300);
+            }
+          }, 100);
+
+          // Remove event listener
+          letter.removeEventListener('click', (e) => {
+            e.target;
+          });
+
+          guessSelector.innerHTML = guessCount;
+        },
+        { once: true }
+      );
+    });
+  },
+  { once: true }
+);
